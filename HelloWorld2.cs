db6a28fdd87adb8d4c227ac64b327d343a318040
nlejeune326 <strakshorktiktok@gmail.com>
2024-12-18 17:30:07 +0100

    j'ai mis un script qui dis dans la console (j'ai fait caca)

---
+++ HelloWorld2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("j'ai fait caca");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
